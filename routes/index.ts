15520e45a66fb4fcb0d1fc66aa2220cdab1e7420
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-11-02 16:29:38 +0200

    Add POST route for /sign-up

--- routes/index.ts
+++ routes/index.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from "express";
+import { userSignUpPost } from "../controllers";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
   res.send("Index page");
 });
+
+router.post("/sign-up", userSignUpPost);
 
 export default router;
