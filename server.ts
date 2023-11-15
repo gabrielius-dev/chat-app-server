121c9a6b8dfb58740a0d7125dce2480caf479ca4
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-11-15 21:06:58 +0200

    Add socket.io server support

--- server.ts
+++ server.ts
@@ -7,10 +7,17 @@ import session from "express-session";
 import mongoose from "mongoose";
 import passport from "passport";
 import indexRouter from "./routes/index";
-
+import http, { Server as HttpServer } from "http";
+import { Server as SocketIOServer, Socket } from "socket.io";
+import { handleAuthentication, handleConnection } from "./controllers/socket";
 dotenv.config();
 
-function createServer(): Application {
+type CustomServer = {
+  app: Application;
+  server: HttpServer;
+};
+
+function createServer(): CustomServer {
   if (process.env.MONGODB_URI) {
     mongoose.connect(process.env.MONGODB_URI);
   } else {
@@ -18,29 +25,52 @@ function createServer(): Application {
   }
 
   const app: Application = express();
+  const server = http.createServer(app);
+  const io = new SocketIOServer(server, {
+    cors: {
+      origin: "http://localhost:5173",
+      credentials: true,
+    },
+  });
 
-  if (process.env.SESSION_SECRET_KEY) {
-    app.use(
-      session({
-        secret: process.env.SESSION_SECRET_KEY,
-        resave: false,
-        saveUninitialized: true,
-        rolling: true,
-        cookie: { maxAge: 24 * 60 * 60 * 1000 },
-      })
-    );
-  } else {
+  let sessionMiddleware;
+  if (process.env.SESSION_SECRET_KEY)
+    sessionMiddleware = session({
+      secret: process.env.SESSION_SECRET_KEY,
+      resave: false,
+      saveUninitialized: true,
+      rolling: true,
+      cookie: { maxAge: 24 * 60 * 60 * 1000 },
+    });
+  else {
     console.error("SESSION_SECRET_KEY environment variable is not defined.");
   }
 
-  initializePassport();
+  if (sessionMiddleware) app.use(sessionMiddleware);
+
   app.use(passport.initialize());
   app.use(passport.session());
+  initializePassport();
   app.use(logger("dev"));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
 
   app.use("/", indexRouter);
+
+  const wrap =
+    <T extends (req: any, res: any, next: (err?: any) => void) => void>(
+      middleware: T
+    ) =>
+    (socket: Socket, next: (err?: any) => void) =>
+      middleware(socket.request, {}, next);
+
+  if (sessionMiddleware) io.use(wrap(sessionMiddleware));
+  io.use(wrap(passport.initialize()));
+  io.use(wrap(passport.session()));
+
+  io.use(handleAuthentication);
+
+  io.on("connection", handleConnection);
 
   // catch 404 and forward to error handler
   app.use((req, res, next) => {
@@ -57,7 +87,7 @@ function createServer(): Application {
     });
   });
 
-  return app;
+  return { app, server };
 }
 
 export default createServer;
