6aa5b9c3861e4d9d387ab7725605f76c45cf252e
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-11-16 22:32:31 +0200

    Add GET /userList route

--- routes/index.ts
+++ routes/index.ts
@@ -1,9 +1,10 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import {
   userLoginPost,
   userSignUpPost,
   userLogOutPost,
   getUserDetails,
+  getUserList,
 } from "../controllers";
 import cors from "cors";
 const router = express.Router();
@@ -15,9 +16,15 @@ const corsForRoutes = cors({
 
 router.options("*", corsForRoutes);
 
+function checkAuth(req: Request, res: Response, next: NextFunction) {
+  if (req.isAuthenticated()) next();
+  else res.status(401).json({ message: "Unauthorized" });
+}
+
 router.post("/sign-up", corsForRoutes, userSignUpPost);
 router.post("/login", corsForRoutes, userLoginPost);
 router.post("/logout", corsForRoutes, userLogOutPost);
 router.get("/user", corsForRoutes, getUserDetails);
+router.get("/userList", corsForRoutes, checkAuth, getUserList);
 
 export default router;
