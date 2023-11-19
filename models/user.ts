6a6134b80fdb16c2e2db964f04cbc1b0af39c597
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-11-19 18:28:14 +0200

    Add img, lastOnline, online fields for userSchema

--- models/user.ts
+++ models/user.ts
@@ -4,6 +4,9 @@ import UserInterface from "./types/user";
 const userSchema = new Schema({
   username: { type: String, required: true, maxLength: 100 },
   password: { type: String, required: true, maxLength: 100 },
+  img: String,
+  lastOnline: { type: Date, default: Date.now },
+  online: { type: Boolean, default: true },
 });
 
 const UserModel = model<UserInterface & Document>("User", userSchema);
