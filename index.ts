91d7b366d8770fdee800f85a895585255894340f
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-11-19 18:19:11 +0200

    Schedule cron checking

--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
 import createServer from "./server";
+import checkOfflineUsers from "./utils/offlineChecker";
+import cron from "node-cron";
 
 const port = process.env.PORT || 8000;
 
 const { app, server } = createServer();
+
+cron.schedule("* * * * *", () => {
+  checkOfflineUsers();
+});
 
 server.listen(port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
