d14dfd52a9fffa5c2b126bcba9f36172c1b9d071
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-11-20 22:20:13 +0200

    Add join-room and send-message routes

--- controllers/socket.ts
+++ controllers/socket.ts
@@ -1,5 +1,7 @@
 import { Socket } from "socket.io";
 import { ExtendedError } from "socket.io/dist/namespace";
+import MessageModel from "../models/message";
+import { io } from "..";
 
 export const handleAuthentication = (
   socket: Socket,
@@ -16,6 +18,31 @@ export const handleAuthentication = (
 
 export const handleConnection = (socket: Socket) => {
   console.log("A user connected");
+
+  socket.on("join-room", (roomId) => {
+    console.log(roomId);
+    socket.join(roomId);
+  });
+
+  socket.on(
+    "send-message",
+    async (
+      message: string,
+      sender: string,
+      receiver: string,
+      roomId: string
+    ) => {
+      const messageObject = new MessageModel({
+        sender,
+        receiver,
+        content: message,
+      });
+      await messageObject.save();
+      console.log(roomId);
+
+      io.to(roomId).emit("receive-message", messageObject);
+    }
+  );
 
   socket.on("disconnect", () => {
     console.log("User disconnected");
