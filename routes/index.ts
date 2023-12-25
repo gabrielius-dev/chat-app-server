1d2530ef34cc483d6858b252913070a95c3a2fd6
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-12-25 21:32:25 +0200

    Add POST /group-chat route

--- routes/index.ts
+++ routes/index.ts
@@ -5,11 +5,13 @@ import {
   userLogOutPost,
   getUserDetails,
   getChatList,
+  createGroupChat,
   getMessages,
   getDatabaseUserDetails,
   editUserDetails,
 } from "../controllers";
 import cors from "cors";
+import multer from "multer";
 const router = express.Router();
 
 const corsForRoutes = cors({
@@ -24,13 +26,22 @@ function checkAuth(req: Request, res: Response, next: NextFunction) {
   else res.status(401).json({ message: "Unauthorized" });
 }
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
 router.post("/sign-up", corsForRoutes, userSignUpPost);
 router.post("/login", corsForRoutes, userLoginPost);
 router.post("/logout", corsForRoutes, checkAuth, userLogOutPost);
 router.get("/user", corsForRoutes, getUserDetails);
 router.get("/user/:id", corsForRoutes, checkAuth, getDatabaseUserDetails);
 router.put("/user/:id", corsForRoutes, checkAuth, editUserDetails);
-router.get("/chatList", corsForRoutes, checkAuth, getChatList);
+router.get("/chat-list", corsForRoutes, checkAuth, getChatList);
+router.post(
+  "/group-chat",
+  corsForRoutes,
+  upload.single("image"),
+  createGroupChat
+);
 router.get("/messages", corsForRoutes, checkAuth, getMessages);
 
 export default router;
