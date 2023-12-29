af95c43ebe767177469db23bcbfc490a07f9195e
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-12-29 22:17:26 +0200

    Add additional interfaces

--- models/types/message.ts
+++ models/types/message.ts
@@ -1,7 +1,23 @@
+import UserInterface from "./user";
+
 export default interface MessageInterface {
-  sender: string;
-  receiver: string;
+  sender: UserInterface;
+  receiver: UserInterface;
   _id: string;
   content: string;
   createdAt: Date;
+}
+
+interface SenderInterface {
+  _id: string;
+  img?: string;
+  username: string;
+}
+
+export interface GroupMessageInterface {
+  _id: string;
+  content: string;
+  createdAt: string;
+  sender: SenderInterface;
+  receiver: string;
 }
