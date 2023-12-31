f097bf62b7b27fbb1fdcbc02cf07454984a650e0
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-12-31 20:13:52 +0200

    Lower groupSchema name field maxLength

--- models/group.ts
+++ models/group.ts
@@ -3,7 +3,7 @@ import GroupInterface from "./types/group";
 
 const groupSchema = new Schema(
   {
-    name: { type: String, required: true, maxLength: 100 },
+    name: { type: String, required: true, maxLength: 25 },
     image: String,
     users: { type: [String], required: true },
     createdAt: { type: Date, default: Date.now },
