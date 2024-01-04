b2964d81ab7574d66ed48a3e4b716a55028d9688
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2024-01-04 21:57:42 +0200

    Add images field to messageSchema

--- models/message.ts
+++ models/message.ts
@@ -1,11 +1,18 @@
 import { Schema, model } from "mongoose";
 import MessageInterface from "./types/message";
 
+const imageSchema = new Schema({
+  width: { type: Number, required: true },
+  height: { type: Number, required: true },
+  url: { type: String, required: true },
+});
+
 const messageSchema = new Schema(
   {
     sender: { type: Schema.Types.ObjectId, ref: "User", required: true },
     receiver: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    content: { type: String, required: true },
+    content: { type: String },
+    images: { type: [imageSchema] },
     createdAt: { type: Date, default: Date.now },
   },
   { versionKey: false }
