f93fe2b65d1774202c5f8e50a003901aa9a57011
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2024-01-07 21:07:45 +0200

    Move socket group chat handlers to index.ts

--- controllers/socket.ts
+++ controllers/socket.ts
@@ -1,10 +1,5 @@
 import { Socket } from "socket.io";
 import { ExtendedError } from "socket.io/dist/namespace";
-import { io } from "..";
-import GroupInterface from "../models/types/group";
-import MessageInterface, {
-  GroupMessageInterface,
-} from "../models/types/message";
 
 export const handleAuthentication = (
   socket: Socket,
@@ -27,97 +22,6 @@ export const handleConnection = (socket: Socket) => {
   socket.on("leave-room", (roomId) => {
     socket.leave(roomId);
   });
-
-  socket.on(
-    "delete-message",
-    async (message: MessageInterface, latestMessage: MessageInterface) => {
-      if (io.sockets.adapter.rooms.get(message.receiver._id.toString())) {
-        socket
-          .to(message.receiver._id.toString())
-          .emit("message-deleted", message);
-        socket
-          .to(message.receiver._id.toString())
-          .emit("message-deleted-chat-list", message, latestMessage);
-      }
-      if (io.sockets.adapter.rooms.get(message.sender._id.toString())) {
-        io.to(message.sender._id.toString()).emit("message-deleted", message);
-        io.to(message.sender._id.toString()).emit(
-          "message-deleted-chat-list",
-          message,
-          latestMessage
-        );
-      }
-    }
-  );
-
-  socket.on(
-    "delete-group-message",
-    async (
-      message: GroupMessageInterface,
-      latestMessage: GroupMessageInterface
-    ) => {
-      if (io.sockets.adapter.rooms.get(message.receiver.toString())) {
-        io.to(message.receiver.toString()).emit(
-          "group-message-deleted",
-          message
-        );
-      }
-      if (
-        io.sockets.adapter.rooms.get(
-          `group-chat-list-${message.receiver.toString()}`
-        )
-      ) {
-        io.to(`group-chat-list-${message.receiver.toString()}`).emit(
-          "group-message-deleted-group-chat-list",
-          message,
-          latestMessage
-        );
-      }
-    }
-  );
-
-  socket.on("create-group-chat", async (groupChat: GroupInterface) => {
-    groupChat.users.forEach((user) => {
-      io.to(user).emit("group-chat-added", {
-        message: `You've been added to the '${groupChat.name}' group chat.`,
-        groupChat,
-      });
-    });
-  });
-
-  socket.on(
-    "edit-group-chat",
-    async (groupChat: GroupInterface, prevGroupChat: GroupInterface) => {
-      io.to(groupChat._id).emit("receive-edit-group-chat", groupChat);
-
-      const removedUsers = prevGroupChat.users.filter(
-        (user) => !groupChat.users.includes(user)
-      );
-
-      const newUsers = groupChat.users.filter(
-        (user) => !prevGroupChat.users.includes(user)
-      );
-
-      removedUsers.forEach((user) => {
-        io.to(user).emit("group-chat-removed", {
-          message: `You've been removed from the '${groupChat.name}' group chat.`,
-          groupChat,
-        });
-      });
-
-      newUsers.forEach((user) => {
-        io.to(user).emit("group-chat-added", {
-          message: `You've been added to the '${groupChat.name}' group chat.`,
-          groupChat,
-        });
-      });
-
-      io.to(`group-chat-list-${groupChat._id}`).emit(
-        "receive-edit-group-chat-list",
-        groupChat
-      );
-    }
-  );
 
   socket.on("disconnect", () => {});
 };
